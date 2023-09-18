cac6eb178d7e24d8db023231be40b5f86be911bd
Lee Jong Hyeok <gurpaper@naver.com>
2023-09-18 20:36:23 +0900

    Memory Pool #1

--- Server Study/ServerCore/CoreMacro.h
+++ Server Study/ServerCore/CoreMacro.h
@@ -18,8 +18,8 @@
 --------------------*/
 
 #ifdef _DEBUG
-#define Xalloc(size)	 StompAllocator::Alloc(size)
-#define Xrelease(ptr)	 StompAllocator::Release(ptr)
+#define Xalloc(size)	 PoolAllocator::Alloc(size)
+#define Xrelease(ptr)	 PoolAllocator::Release(ptr)
 #else
 #define Xalloce(size)	BaseAllocator::Alloc(size)
 #define Xreleasee(pte)	BaseAllocator::Release(ptr)
