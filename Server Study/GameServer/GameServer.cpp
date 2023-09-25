001a7567303646ab3eea7b0388ac72b1a8a8ec14
Lee Jong Hyeok <gurpaper@naver.com>
2023-09-25 10:06:20 +0900

    SLIST_ENTRY 사용

--- Server Study/GameServer/GameServer.cpp
+++ Server Study/GameServer/GameServer.cpp
@@ -11,25 +11,24 @@
 #include "RefCounting.h"
 #include "Memory.h"
 #include "Allocator.h"
-#include "LockFreeStack.h"
 
 DECLSPEC_ALIGN(16)
 class Data	// :public SListEntry
 {
 public:
-	SListEntry _entry;
+	SLIST_ENTRY _entry;
 
 	int64 _rand = rand() % 1000;
 };
 
-SListHeader* GHeader;
+SLIST_HEADER* GHeader;
 
 int main()
 {
-	GHeader = new SListHeader();
+	GHeader = new SLIST_HEADER();
 	ASSERT_CRASH(((uint64)GHeader % 16) == 0);
 
-	InitializeHead(GHeader);
+	::InitializeSListHead(GHeader);
 
 	for (int32 i = 0; i < 3; ++i)
 	{
@@ -39,7 +38,7 @@ int main()
 				Data* data = new Data();
 				ASSERT_CRASH(((uint64)data % 16) == 0);
 
-				PushEntrySList(GHeader, (SListEntry*)data);
+				::InterlockedPushEntrySList(GHeader, (SLIST_ENTRY*)data);
 				this_thread::sleep_for(10ms);
 			}
 		});
@@ -51,7 +50,7 @@ int main()
 			while (true)
 			{
 				Data* pop = nullptr;
-				pop = (Data*)PopEntrySList(GHeader);
+				pop = (Data*)::InterlockedPopEntrySList(GHeader);
 
 				if (pop)
 				{
@@ -60,7 +59,7 @@ int main()
 				}
 				else
 				{
-					cout << "NONE" << endl;
+					//cout << "NONE" << endl;
 				}
 			}
 		});
