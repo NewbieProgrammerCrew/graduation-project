4862949f7e2341db7a39ad0facf1d92eb0fc89b8
Lee Jong Hyeok <gurpaper@naver.com>
2023-10-05 20:34:41 +0900

    소켓 옵션

--- Server Study/GameServer/GameServer.cpp
+++ Server Study/GameServer/GameServer.cpp
@@ -8,100 +8,93 @@
 #include <future>
 #include "ThreadManager.h"
 
-#include "RefCounting.h"
-#include "Memory.h"
-#include "Allocator.h"
+#include <WinSock2.h>
+#include <MSWSock.h>
+#include <ws2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")
 
-using TL = TypeList<class Player, class Mage, class Knight, class Archer>;
-
-class Player
+void HandleError(const char* cause)
 {
-public:
-	Player()
-	{
-		INIT_TL(Player);
-	}
-
-	virtual ~Player() {};
-
-	DECLARE_TL
-};
-
-class Knight : public Player
-{
-public:
-	Knight() { INIT_TL(Knight) }
-
-};
-
-class Mage : public Player
-{
-public:
-	Mage() { INIT_TL(Mage) }
-};
-
-class Archer : public Player
-{
-public:
-	Archer() { INIT_TL(Archer) }
-};
-
-class Dog
-{
-
-};
+	int32 errCode = ::WSAGetLastError();
+	cout << cause << "ErrorCode : " << errCode << endl;
+}
 
 
 int main()
 {
-	/*TypeList<Mage, Knight>::Head whoAMI;
-	TypeList<Mage, Knight>::Tail whoAMI2;
+	WSAData wsaData;
+	if (::WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+		return 0;
 
-	TypeList < Mage, TypeList<Knight, Archer>>::Head whiMAI3;
-	TypeList < Mage, TypeList<Knight, Archer>>::Tail::Head whiMAI4;
-	TypeList < Mage, TypeList<Knight, Archer>>::Tail::Tail whiMAI5;
-
-	int len1 = Length<TypeList<Mage, Knight>>::value;
-	int len2 = Length<TypeList<Mage, Knight, Archer>>::value;
-
-	TypeAt<	TL, 0> ::Result whoAMI6;
-	TypeAt<	TL, 1> ::Result whoAMI7;
-	TypeAt<	TL, 2> ::Result whoAMI8;
-
-	int32 index1 = IndexOf<TL, Mage>::value;
-	int32 index2 = IndexOf<TL, Archer>::value;
-	int32 index3 = IndexOf<TL, Dog>::value;
-
-	bool canConvert1 = Conversion<Player, Knight>::exists;
-	bool canConvert2 = Conversion<Knight, Player>::exists;
-	bool canConvert3 = Conversion<Knight, Dog>::exists;*/
-
-	/*{
-		Player* player = new Player();
-
-		bool canCast = CanCast<Knight*>(player);
-		Knight* knight = TypeCast<Knight*>(player);
-
-		delete player;
-	}*/
-
+	SOCKET serverSocket = ::socket(AF_INET, SOCK_DGRAM, 0);
+	if (serverSocket == INVALID_SOCKET)
 	{
-		shared_ptr<Player> player = MakeShared<Knight>();
-
-		shared_ptr<Player> archer = TypeCast<Archer>(player);
-		bool canCast = CanCast<Mage>(player);
+		HandleError("Socket");
+		return 0;
 	}
 
-	for (int32 i = 0; i < 5; ++i)
+	// 옵션을 해석하고 처리할 주체?
+	// 소켓 코드 -> SQL_SOCKET
+	// IPv4 -> IPPROTO_IP
+	// TCP 프로토콜 -> IPPROTO_TCP
+
+	// SO_KEEPALIVE = 주기적으로 연결 상태 확인 여부 (TCP Only)
+	// 상대방이 소리소문없이 연결 끊는다면?
+	// 주기적으로 TCP 프로토콜 연결 상태 확인 -> 끊어진 연결 감지
+
+	bool enable = true;
+	::setsockopt(serverSocket, SOL_SOCKET, SO_KEEPALIVE,(char*)&enable,sizeof(enable));
+
+	// SO_LINGER = 지연하다
+	// 송신 버퍼에 있는 데이터를 보낼 것인가? 날릴 것인가?
+	
+	// onoff = 0이면 closesocket()이 바로 리턴, 아니면 linger초만큼 대기 (default 0)
+	// linger : 대기 시간
+
+	LINGER linger;
+	linger.l_onoff = 1;
+	linger.l_linger = 5;
+	::setsockopt(serverSocket, SOL_SOCKET, SO_LINGER, (char*)&linger, sizeof(linger));
+
+	// Half-Close
+	// SD_SEND : send 막는다
+	// SD_RECEIVE : recv 막는다
+	// SD_BOTH : 둘다 막는다
+	//::shutdown(serverSocket, SD_SEND);
+	// 소켓 리소스 반환
+	// send -> closesocket
+	//::closesocket(serverSocket);
+
+	// SO_SNDBUF = 송신 버퍼 크기
+	// SO_RCVBUF = 수신 버퍼 크기
+
+	int32 sendBufferSize;
+	int32 optionLen = sizeof(sendBufferSize);
+	::getsockopt(serverSocket, SOL_SOCKET, SO_SNDBUF, (char*)&sendBufferSize, &optionLen);
+	cout << "송신 버퍼 크기 : " << sendBufferSize << endl;
+
+	int32 recvBufferSize;
+	optionLen = sizeof(recvBufferSize);
+	::getsockopt(serverSocket, SOL_SOCKET, SO_RCVBUF, (char*)&recvBufferSize, &optionLen);
+	cout << "수신 버퍼 크기 : " << recvBufferSize << endl;
+
+	// SO_REUSEADDR
+	// IP주소 및 port 재사용
 	{
-		GThreadManager->Launch([]() {
-			while (true)
-			{		
-				
-			}
-		});
+		bool enable = true;
+		::setsockopt(serverSocket, SOL_SOCKET, SO_REUSEADDR, (char*)&enable, sizeof(enable));
 	}
 
+	// IPPROTO_TCP
+	// TCP_NODELAY = Nagle 네이글 알고리즘 작동 여부
+	// 데이터가 충분히 크면 보내고, 그렇지 않으면 데이터가 충분히 쌓일 떄 까지 대기!
+	// 장점 : 작은 패킷이 불필요하게 많이 생성되는 일을 방지
+	// 단점 : 반응 시간 손해
+	{
+		bool enbale = true;
+		::setsockopt(serverSocket, IPPROTO_TCP, TCP_NODELAY, (char*)&enable, sizeof(enable));
+	}
 
-	GThreadManager->Join();
+	// 윈속 종료
+	::WSACleanup();
 }
