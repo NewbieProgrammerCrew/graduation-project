d56da011f19c7c087fd2096bcfe8872b50ec329b
Lee Jong Hyeok <gurpaper@naver.com>
2023-10-07 01:25:17 +0900

    클라이언트 에서 이동 관리 하도록 수정

--- Server/ServerTest/ServerTest/protocol.h
+++ Server/ServerTest/ServerTest/protocol.h
@@ -9,55 +9,76 @@ constexpr int W_HEIGHT = 8;
 // Packet ID
 constexpr char CS_LOGIN = 0;
 constexpr char CS_MOVE = 1;
+constexpr char CS_ATTACK = 2;
 
 constexpr char SC_LOGIN_INFO = 2;
 constexpr char SC_ADD_PLAYER = 3;
 constexpr char SC_REMOVE_PLAYER = 4;
 constexpr char SC_MOVE_PLAYER = 5;
+constexpr char SC_CHANGE_HP = 6;
+constexpr char SC_ATTACK = 7;
 
 #pragma pack (push, 1)
 struct CS_LOGIN_PACKET {
-    unsigned char size;
-    char    type;
-    char    name[PROTOCOL_NAME_SIZE];
+	unsigned char	size;
+	char			type;
+	char			name[PROTOCOL_NAME_SIZE];
 };
 
 struct CS_MOVE_PACKET {
-    unsigned char size;
-    char    type;
-    float   rx, ry, rz;
-    short   direction;  // 0 : Fwd , 1 : back, 2 : LEFT, 3 : RIGHT 4: jump 5
-    float   speed;
+	unsigned char	size;
+	char			type;
+	float			rx, ry, rz;
+	float			x, y, z;
+	float			speed;
+};
+
+struct CS_ATTACK_PACKET {
+	unsigned char	size;
+	char			type;
+	int				id;
+	float			ry;
+	float			x, y, z;
 };
 
 struct SC_LOGIN_INFO_PACKET {
-    unsigned char size;
-    char        type;
-    int         id;
-    float    x, y, z;
+	unsigned char	size;
+	char			type;
+	int				id;
+	float			x, y, z;
 };
 
 struct SC_ADD_PLAYER_PACKET {
-    unsigned char size;
-    char    type;
-    int    id;
-    float    x, y, z;
-    char    name[PROTOCOL_NAME_SIZE];
+	unsigned char	size;
+	char			type;
+	int				id;
+	int				_hp;
+	float			x, y, z;
+	char			name[PROTOCOL_NAME_SIZE];
 };
 
 struct SC_REMOVE_PLAYER_PACKET {
-    unsigned char size;
-    char    type;
-    int    id;
+	unsigned char	size;
+	char			type;
+	int				id;
 };
 
 struct SC_MOVE_PLAYER_PACKET {
-    unsigned char size;
-    char    type;
-    int    id;
-    float    x, y, z;
-    float   rx, ry, rz;
-    float speed;
+	unsigned char	size;
+	char			type;
+	int				id;
+	float			x, y, z;
+	float			rx, ry, rz;
+	float			speed;
 };
+
+struct SC_CAHNGE_HP_PACKET {
+	unsigned char   size;
+	char            type;
+	int             id;
+	int             _hp;
+};
+
+
 
 #pragma pack (pop)
