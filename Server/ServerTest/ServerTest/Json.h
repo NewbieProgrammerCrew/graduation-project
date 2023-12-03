e5f4a8dd0d239f1f38137433d2129299f1df3b6b
JeongHun <45549589+ojh6507@users.noreply.github.com>
2023-12-03 14:37:02 +0900

    경로수정

--- Server/ServerTest/ServerTest/Json.h
+++ Server/ServerTest/ServerTest/Json.h
@@ -29,17 +29,17 @@ extern map<int,objects> ST2_OBJ;
 extern map<int,objects> ST3_OBJ;
 
 
-
-"BP_Jelly_C_0_955AE44741817DFD7815F9B7BF37C6AD": [
-{
-	"Type": "Box",
-		"LocationX" : 1270.0370060111857,
-		"LocationY" : -2.9612410164232776,
-		"LocationZ" : 91.148533319730404,
-		"ExtentX" : 93.120000064373016,
-		"ExtentY" : 96,
-		"ExtentZ" : 92.160000085830688,
-		"Yaw" : 0,
-		"Roll" : -0,
-		"Pitch" : 0
-}
+//
+//"BP_Jelly_C_0_955AE44741817DFD7815F9B7BF37C6AD": [
+//{
+//	"Type": "Box",
+//		"LocationX" : 1270.0370060111857,
+//		"LocationY" : -2.9612410164232776,
+//		"LocationZ" : 91.148533319730404,
+//		"ExtentX" : 93.120000064373016,
+//		"ExtentY" : 96,
+//		"ExtentZ" : 92.160000085830688,
+//		"Yaw" : 0,
+//		"Roll" : -0,
+//		"Pitch" : 0
+//}
