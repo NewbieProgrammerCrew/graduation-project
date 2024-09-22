f0e4297d0763c2ba3de56eb942c5ed64a6011b8d
JeongHun <45549589+ojh6507@users.noreply.github.com>
2024-09-23 05:00:21 +0900

    [클라] 아이피 설정 위젯 추가(시연용)
    아이피 최초로 한번만 설정하면 됨.

---
+++ UnrealProject/SampleServer_Unreal 5.3/Source/NPC_World/Public/Widget/SettingNetworkWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "SettingNetworkWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NPC_WORLD_API USettingNetworkWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
